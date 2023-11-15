bc3d8a946c4f01c0164d52fb3adbfbde4a20c71f
villageworks <villageworks@gmail.com>
2023-11-15 17:29:45 +0900

    ver.0.11: 「安い順」での並替え機能の実装

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Item;
 import com.example.demo.repository.ItemRepository;
@@ -17,13 +18,24 @@ public class ItemController {
 	ItemRepository itemRepository;
 	
 	// 初期表示
-	@GetMapping("items")
-	public String index(Model model) {
-		// すべての商品リストを取得
-		List<Item> list = itemRepository.findAll();
-		// スコープに商品リストを登録
-		model.addAttribute("itemList", list);
+	@GetMapping("/items")
+	public String index(
+			@RequestParam(name = "sort", defaultValue = "") String sort,
+			Model model) {
+		// sortキーによって処理を分割
+		if (sort.isEmpty()) {
+			// すべての商品リストを取得
+			List<Item> list = itemRepository.findAll();
+			// スコープに商品リストを登録
+			model.addAttribute("itemList", list);
+		} else {
+			// 指定された並び順の商品リストを取得
+			List<Item> list = itemRepository.findAllByOrderByPriceAsc();
+			// スコープに商品リストを登録
+			model.addAttribute("itemList", list);
+		}
 		// 画面遷移
 		return "items";
 	}
+	
 }
--- src/main/java/com/example/demo/repository/ItemRepository.java
+++ src/main/java/com/example/demo/repository/ItemRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
+
+	// 価格の安い順（昇順）に商品を取得する
+	List<Item> findAllByOrderByPriceAsc();
+
 
 }
